dc5b48e9f2e87ba1e6de12f1eb085cec282400a5
kimseongjung <plznw4me@naver.com>
2018-08-09 16:36:22 +0900

    strike a balance btw fg and bg

---
+++ eval.py
@@ -0,0 +1,24 @@
+import tensorflow as tf
+
+# Reconstruct Model
+
+
+
+# Change Input placeholder to [None , None ,None , 3 ]
+
+
+
+
+
+# Get Activation Map
+
+
+
+
+# Visualization
+
+
+
+
+# Calculate 
+
